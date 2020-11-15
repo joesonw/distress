eda7ce89015c6a42db79fe1570cd452f6db0e5e4
Qiaosen Huang <joesonw@gmail.com>
2020-11-15 22:53:34 +0800

    :recycle: :bug: rename cmd and fix non-absolute directory path usage

---
+++ cmd/ds-agent/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+
+	"github.com/joesonw/distress/cmd/ds-agent/app"
+)
+
+func main() {
+
+	var logger *zap.Logger
+	rootCmd := &cobra.Command{
+		Use:   "distress-ds-agent",
+		Short: "distributed stress ds-agent",
+	}
+
+	pDebug := rootCmd.PersistentFlags().Bool("debug", false, "enable debug mode")
+	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
+		var err error
+		if *pDebug {
+			logger, err = zap.NewDevelopment()
+		} else {
+			logger, err = zap.NewProduction()
+		}
+		return err
+	}
+
+	rootCmd.AddCommand(app.MakeCmdRun(&logger, pDebug))
+
+	err := rootCmd.Execute()
+	if err != nil {
+		println(err)
+		os.Exit(1)
+	}
+}
